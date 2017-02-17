2891ea15c0e160f2d31f6cad299d87699efbdeee
Sim Boon Long <simboonlong@gmail.com>
2017-02-17 17:31:54 +0800

    UPDATE: drop tweenmax for animate.css
    refactor code

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 ## scrollolo.js
-A lightweight js plugin, that animates elements when entering viewport.
+Animates elements when entering viewport.
 
-1. requires Jquery, TweenMax.
-2. manually tested on latest version of Chrome, Firefox, Safari.
+1. requires Jquery, Animate.css.
+2. manually tested on latest version of Chrome, Firefox, Safari, IE.
 3. optional custom animations, using data-attributes.
 
 
@@ -13,10 +13,9 @@ Demo link at http://htmlpreview.github.io/?https://github.com/simboonlong/scroll
 
 
 ## Mechanics
-
-Currently the plugin only supports the 4 basic animations: translate, scale, rotation, opacity. This is in line with keeping animations high performing on browsers. Read more at: http://www.html5rocks.com/en/tutorials/speed/high-performance-animations/
-
-Integrated throttling function from underscore.js. Read more at: http://underscorejs.org/#throttle
+- RAF scrolling checks if scrollolo element is within viewport.
+- Add animation css class when in viewport.
+- Default transition is fading in.
 
 
 ## Usage
@@ -30,62 +29,29 @@ To use, assign `scrollolo` class to any element you want to animate.
 Include `scrollolo.js` script.
 ```
 
-<script type="text/javascript" src="scrollolo.js"></script>
+<script type="text/javascript" src="js/scrollolo.js"></script>
 
 ```
 
-Then initialize `scrollolo.js` with the following arguments.
+Then initialize `scrollolo.js`.
 
 ```
 
-// instantiate with scrollolo class + options
-var scrolloloCache = $('.scrollolo'); // to cache element
-var scrollolo = new Scrollolo( scrolloloCache, {
-    throttleThreshold : 20, // scrolling throttle
-    offsetDown : 0, // element percentage offset for scrolling down, use float values
-    offsetUp : 0, // element percentage offset for scrolling up, use float values
-    direction : 'none', // x, y, none
-    x : 20, // starting x offset
-    y : 20, // starting y offset
-    scale : 0.2, // starting scale
-    rotation : 0, // starting rotation
-    opacity : 0, // starting opacity
-    delay : 0, // starting delay
-    duration : 1.7, // starting duration
-    easeType : 'Expo.easeOut' // ease type, refer to TweenMax documentation at: http://greensock.com/ease-visualizer
-});
-scrollolo.initialize(); // init scrollolo
+Scrollolo.init();
 
 ```
 
 ## Customizing
-Use data attributes `data-scrollolo` to do customized animation behaviour on any element with the scrollolo class. Below are the available data attributes:
-
-- data-scrollolo-direction
-- data-scrollolo-x
-- data-scrollolo-y
-- data-scrollolo-scale
-- data-scrollolo-rotation
-- data-scrollolo-opacity
-- data-scrollolo-delay
-- data-scrollolo-duration
-- data-scrollolo-easetype
+Use data attributes `data-animation` to do customized animation behaviour on any element with the scrollolo class.
 
 Example:
 ```
 
-<div class="scrollolo" data-scrollolo-direction="y" data-scrollolo-y="100" data-scrollolo-scale="1" data-scrollolo-rotation="360" data-scrollolo-opacity="0.25" data-scrollolo-delay="0.7" data-scrollolo-duration="1.5" data-scrollolo-easetype="Expo.easeOut">
+<div class="scrollolo" data-animation="bounceInDown">
 
 ```
 
-
-## Issues
-1. Not tested in IE yet.
-
-
-## Todo
-1. Trigger on elements' offset position relative to document.height, rather than isElementInViewport.
-2. Offset capability for individual elements.
+Please refer to [documentation](https://github.com/daneden/animate.css) of animate.css by Daniel Eden.
 
 
 ## License
---
+++ js/scrollolo.js
@@ -0,0 +1,133 @@
+/*!
+scrollolo.js - http://simboonlong.com
+Licensed under the MIT license
+
+Copyright (c) 2017 Sim Boon Long
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+
+var Scrollolo = (function() {
+
+    var settings = {};
+
+    var raf = function( cb ) {
+
+        var lastTime = 0;
+        var vendors = ['ms', 'moz', 'webkit', 'o'];
+
+        for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+            window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+            window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame'] || window[vendors[x]+'CancelRequestAnimationFrame'];
+        }
+
+        if (!window.requestAnimationFrame) {
+            window.requestAnimationFrame = function(callback, element) {
+                var currTime = new Date().getTime();
+                var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+                var id = window.setTimeout(function() { callback(currTime + timeToCall); },
+                  timeToCall);
+                lastTime = currTime + timeToCall;
+                return id;
+            };
+        }
+
+        if (!window.cancelAnimationFrame) {
+            window.cancelAnimationFrame = function(id) {
+                clearTimeout(id);
+            };
+        }
+    };
+
+
+    var rafScroll = function( cb ) {
+
+        var lastPosition = -1;
+        var render = function() {
+            // Avoid calculations if not needed
+            if (lastPosition === window.pageYOffset) {
+                return false;
+            } else {
+                lastPosition = window.pageYOffset;
+            }
+
+            cb();
+        };
+        var draw = function() {
+            requestAnimationFrame(draw);
+            render();
+        };
+
+
+        var lastTime = 0;
+        var vendors = ['ms', 'moz', 'webkit', 'o'];
+
+        for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+            window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+            window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame'] || window[vendors[x]+'CancelRequestAnimationFrame'];
+        }
+
+        if (!window.requestAnimationFrame) {
+            window.requestAnimationFrame = function(callback, element) {
+                var currTime = new Date().getTime();
+                var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+                var id = window.setTimeout(function() { callback(currTime + timeToCall); },
+                  timeToCall);
+                lastTime = currTime + timeToCall;
+                return id;
+            };
+        }
+
+        if (!window.cancelAnimationFrame) {
+            window.cancelAnimationFrame = function(id) {
+                clearTimeout(id);
+            };
+        }
+
+        draw();
+
+    };
+
+    var isElementInViewport = function (el) {
+        if (typeof jQuery === "function" && el instanceof jQuery) {
+            el = el[0];
+        }
+
+        var rect = el.getBoundingClientRect();
+
+        return (
+            rect.top >= 0 &&
+            rect.left >= 0 &&
+            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && /*or $(window).height() */
+            rect.right <= (window.innerWidth || document.documentElement.clientWidth) /*or $(window).width() */
+        );
+    };
+
+    var checkScrollolo = function() {
+        settings.scrollolo.each(function(i) {
+            var el = $(this);
+            if ( isElementInViewport( this ) ) {
+                el.addClass('is-active');
+                if ( el.attr('data-animation') !== undefined && el.attr('data-animation') !== null ) {
+                    el.addClass('animated ' + el.attr('data-animation') );
+                }
+            }
+        });
+    };
+
+    var init = function( options ) {
+        settings.scrollolo = $('.scrollolo');
+        rafScroll( checkScrollolo );
+    };
+
+    return {
+        init: init,
+        settings: settings
+    };
+
+}());
